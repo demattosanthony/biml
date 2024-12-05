33c1eb39c7d2dcb979703a71e8f9f4d0f6112e6f
Anthony DeMattos <demattosanthony@gmail.com>
2024-12-05 16:02:37 -0500

    copying of materials over

--- saron/editor.py
+++ saron/editor.py
@@ -3,6 +3,9 @@ from ifcopenshell.api import root, context, unit, spatial, type, geometry, aggre
 from ifcopenshell import guid
 import ifcopenshell.api
 import ifcopenshell.api.geometry
+import ifcopenshell.api.material
+import ifcopenshell.util
+import ifcopenshell.util.element
 import numpy as np
 import time
 
@@ -88,14 +91,15 @@ model.create_entity("IfcRelAggregates", GlobalId=guid.new(), RelatingObject=buil
 # Load source IFC with the door
 source_file = ifcopenshell.open("bim_objects/DoorPanel_Aluminum_Cline_Louver-TopAndBottom.ifc")
 original_door = source_file.by_type("IfcDoor")[0]
+original_door_materials = ifcopenshell.util.element.get_materials(original_door)
 
 # Copy the door into the new model
 new_door = ifcopenshell.util.element.copy_deep(model, original_door)
-new_door_2 = ifcopenshell.util.element.copy_deep(model, original_door)
 
 # Update the GlobalId and Name
 new_door.GlobalId = guid.new()
 new_door.Name = "Office Door"
+new_door.Description = "This is an office door that was copied from another IFC file."
 
 door_placement = model.create_entity(
     "IfcLocalPlacement",
@@ -103,15 +107,26 @@ door_placement = model.create_entity(
 )
 new_door.ObjectPlacement = door_placement
 
-door_2_placement = model.create_entity(
-    "IfcLocalPlacement",
-    RelativePlacement=model.create_entity("IfcAxis2Placement3D", Location=model.create_entity("IfcCartesianPoint", Coordinates=(0.0, 1.0, 0.0))),
-)
-new_door_2.ObjectPlacement = door_2_placement
+# Get original door materials
+orig_materials = ifcopenshell.util.element.get_materials(original_door)
+for orig_mat in orig_materials:
+    # Deep copy the material from the source file to the new model
+    new_mat = ifcopenshell.util.element.copy_deep(model, orig_mat)
+
+    # Check for any material definition representations that define color
+    if hasattr(orig_mat, "HasRepresentation") and orig_mat.HasRepresentation:
+        for mdr in orig_mat.HasRepresentation:
+            if mdr.is_a("IfcMaterialDefinitionRepresentation"):
+                # Deep copy the material definition representation
+                new_mdr = ifcopenshell.util.element.copy_deep(model, mdr)
+                # Link the new Mdr to the new material
+                new_mdr.RepresentedMaterial = new_mat
+
+    # Assign the newly copied material (with its representation) to the new door
+    ifcopenshell.api.material.assign_material(file=model, products=[new_door], material=new_mat)
 
 # Set spatial containment
 model.create_entity("IfcRelContainedInSpatialStructure", GlobalId=guid.new(), RelatingStructure=storey, RelatedElements=[new_door])
-model.create_entity("IfcRelContainedInSpatialStructure", GlobalId=guid.new(), RelatingStructure=storey, RelatedElements=[new_door_2])
 
 # Save the model
 model.write("test.ifc")
