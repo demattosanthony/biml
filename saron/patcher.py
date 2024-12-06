71719234195e89193f5595ca9d8c3a4bbcd5dac9
Anthony DeMattos <demattosanthony@gmail.com>
2024-12-06 16:27:00 -0500

    swotched to patcher

--- saron/patcher.py
+++ saron/patcher.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Migrate an IFC file to a different schema.")
 parser.add_argument("--input", help="The input IFC file.")
 parser.add_argument("--output", help="The output IFC file.")
+parser.add_argument("--recipe", default="Migrate", help="The recipe to use for the migration.")
+parser.add_argument("--arguments", default="IFC4", help="The arguments to pass to the recipe.")
 
 args = parser.parse_args()
 assert args.input, "An input IFC file must be provided."
@@ -12,6 +14,6 @@ assert args.output, "An output IFC file must be provided."
 
 model = ifcopenshell.open(args.input)
 
-output = ifcpatch.execute({"input": "input.ifc", "file": model, "recipe": "Migrate", "arguments": ["IFC4"]})
+output = ifcpatch.execute({"input": "input.ifc", "file": model, "recipe": args.recipe, "arguments": [args.arguments]})
 
 ifcpatch.write(output, args.output)
