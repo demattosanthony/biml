a6e116ef11e2806c433b5b62ead3ed21057d8be3
Anthony DeMattos <demattosanthony@gmail.com>
2024-12-09 16:52:27 -0500

    moved some things around

--- saron/saron/ifc_session.py
+++ saron/saron/ifc_session.py
@@ -4,8 +4,6 @@ from ifcopenshell.api import context, aggregate, owner
 import ifcopenshell.api
 import ifcopenshell.api.owner
 
-from saron.utils import build_hierarchy 
-
 class IfcSession:
     def __init__(self) -> None:
         self.file: ifcopenshell.file = None
@@ -94,5 +92,74 @@ class IfcSession:
                 content += f"      * {item}\n"
 
         return tree
-            
+
+def build_hierarchy(project: ifcopenshell.entity_instance, with_properties=False):
+    """Build the hierarchy of the IFC project
     
+    Args:
+        project (ifcopenshell.entity_instance): The IFC project
+        with_properties (bool, optional): Include properties. Defaults to False.
+    
+    Returns:
+        str: The hierarchy of the IFC project
+    """
+    result = []
+    spacer = '.  '
+    
+    def add_line(text, level):
+        result.append(spacer * level + text)
+    
+    def add_property_set(property_set, level):
+        add_line(property_set.Name, level)
+        for prop in property_set.HasProperties:
+            if prop.is_a('IfcPropertySingleValue'):
+                add_line(f"{prop.Name} = {str(prop.NominalValue.wrappedValue)}", level + 1)
+
+    def add_quantity_set(quantity_set, level):
+        add_line(quantity_set.Name, level)
+        for quantity in quantity_set.Quantities:
+            if quantity.is_a('IfcQuantityLength'):
+                add_line(f"{quantity.Name} = {str(quantity.LengthValue)}", level + 1)
+            elif quantity.is_a('IfcQuantityArea'):
+                add_line(f"{quantity.Name} = {str(quantity.AreaValue)}", level + 1)
+            elif quantity.is_a('IfcQuantityVolume'):
+                add_line(f"{quantity.Name} = {str(quantity.VolumeValue)}", level + 1)
+            elif quantity.is_a('IfcQuantityCount'):
+                add_line(f"{quantity.Name} = {str(quantity.CountValue)}", level + 1)
+            else:
+                add_line(quantity.Name, level + 1)
+
+    def add_element_type(type, level):
+        add_line(type.Name, level)
+
+    def add_element(element, level):
+        add_line(f"#{element.id()} = {element.is_a()} \"{element.Name}\" ({element.GlobalId})", level)
+        
+        for definition in element.IsDefinedBy:
+            if with_properties:
+                if definition.is_a('IfcRelDefinesByProperties'):
+                    related_data = definition.RelatingPropertyDefinition
+                    if related_data.is_a('IfcPropertySet'):
+                        add_property_set(related_data, level + 1)
+                    elif related_data.is_a('IfcElementQuantity'):
+                        add_quantity_set(related_data, level + 1)
+            if definition.is_a('IfcRelDefinesByType'):
+                add_element_type(definition.RelatingType, level + 1)
+
+        # Spatial relation
+        if element.is_a('IfcSpatialStructureElement'):
+            for rel in element.ContainsElements:
+                for child in rel.RelatedElements:
+                    add_element(child, level + 1)
+
+        # Aggregation Relation
+        if element.is_a('IfcObjectDefinition'):
+            for rel in element.IsDecomposedBy:
+                for child in rel.RelatedObjects:
+                    add_element(child, level + 1)
+
+    # Main execution
+    for item in project:
+        add_element(item, 0)
+    
+    return '\n'.join(result)
